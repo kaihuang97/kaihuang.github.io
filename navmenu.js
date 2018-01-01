885f1ff44a803c865b2f57c88eba87d00fe176b7
Kai Huang <Kai@Kais-MacBook-Pro.local>
2017-12-31 22:05:19 -0800

    added navbar

---
+++ navmenu.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.getElementById("nav").style.width = "200px";
+    document.getElementById("main").style.marginRight = "200px";
+}
+
+function closeMenu() {
+    document.getElementById("nav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+}
